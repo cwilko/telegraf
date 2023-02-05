200f3ea00fe10cd18a77eb3c3b2e9573f5ded94e
cwilko <chris.wilkinson@uk.ibm.com>
2023-02-05 22:06:21 +0000

    Migrate to Github Actions

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,11 @@ if [ "${1:0:1}" = '-' ]; then
     set -- telegraf "$@"
 fi
 
-exec "$@"
+if [ "$(id -u)" -ne 0 ]; then
+    exec "$@"
+else
+    # Allow telegraf to send ICMP packets and bind to privliged ports
+    setcap cap_net_raw,cap_net_bind_service+ep /usr/bin/telegraf || echo "Failed to set additional capabilities on /usr/bin/telegraf"
+
+    exec su-exec telegraf "$@"
+fi
